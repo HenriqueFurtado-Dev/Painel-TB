3ec2cef395d30e9f26683fb80b572c853418cfb5
Henrique Furtado <63440926+HenriqueFurtado-Dev@users.noreply.github.com>
2022-12-09 19:50:14 -0300

    Add model viewer / Workstation

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,56 @@ btnCopy.addEventListener( 'click', function(){
   
   return false;
 } );
+
+
+
+
+
+
+let editor = document.querySelector("#editor");
+
+ace.edit(editor, {
+  theme: "ace/theme/cobalt",
+  mode: "ace/mode/javascript",
+});
+
+
+
+
+
+
+
+
+const popupTrigger = document.querySelector('.popup-trigger');
+const popup = document.querySelector('.popup');
+const popupClose = document.querySelector('.popup__close');
+
+popupTrigger.addEventListener('click', (e) => {
+  popup.classList.add('show');
+  document.body.style.cssText = `overflow: hidden;`;
+
+});
+
+popupClose.addEventListener('click', (e) => {
+  popup.classList.remove('show');
+  document.body.style.cssText = '';
+});
+
+// close on click on overlay
+
+popup.addEventListener('click', (e) => {
+  if (e.target === popup) {
+    popup.classList.remove('show');
+    document.body.style.cssText = '';
+  }
+});
+
+// close on press of escape button
+
+document.addEventListener('keydown', (e) => {
+    if (e.code === "Escape" && popup.classList.contains('show')) {
+      popup.classList.remove('show');
+      document.body.style.cssText = '';
+    }
+  });
+
